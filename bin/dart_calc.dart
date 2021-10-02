59dea39e0fdccdf341d002798c7b09c66fa27cb6
Jim Hessin <jhessin@gmail.com>
2021-10-02 15:10:23 -0600

    simple update

--- bin/dart_calc.dart
+++ bin/dart_calc.dart
@@ -1,5 +1,5 @@
 import 'package:dart_calc/dart_calc.dart' as dart_calc;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_calc.calculate()}!');
+  dart_calc.run();
 }
--- lib/dart_calc.dart
+++ lib/dart_calc.dart
@@ -1,3 +1,7 @@
 int calculate() {
   return 6 * 7;
 }
+
+run() {
+  print('Pick favorite color:');
+}
